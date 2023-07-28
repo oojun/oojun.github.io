36543661c25dd662a6137762d865dd368e0f47e1
Jun198 <76256558+Jun198@users.noreply.github.com>
2023-07-28 14:31:26 +0800

    更新 hello-world.md Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/1243066710.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["1243066710.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
